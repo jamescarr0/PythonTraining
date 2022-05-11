a6520070f7856a7a4f43da2450c9dfbd26e42081
jamescarr0 <jamescarr0@hotmail.com>
2022-05-11 21:47:16 +0100

    sets exercise complete

---
+++ Coding_Interview_Tips_Best_Practices/Core_Data_Structures/sets.py
@@ -0,0 +1,23 @@
+def count_unique(s):
+    '''
+    Count the number of unique characters in s
+    
+    >>> count_unique("aabb")
+    2
+
+    >>> count_unique("abcdef")
+    6
+    '''
+
+    # O(1) Constant time, set can take an iterable in its constructor.
+    return len(set(s))
+
+    # O(1) using set comprehension.
+    # return len({c for c in s})
+
+    # O(n * n) = O(n^2)
+    # seen_c = []                 # O(1)
+    # for c in s:                 # O(n)
+    #     if c not in seen_c:     # O(n)
+    #         seen_c.append(c)    # O(1)
+    # return len(seen_c)          # O(1)
