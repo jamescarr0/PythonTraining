75acbec5f65cd275abda1351d5d45e8d5796ab3b
jamescarr0 <jamescarr0@hotmail.com>
2022-06-01 23:07:53 +0100

    DRY; Create base.html & extend templates complete

--- Django_Portfolio_app/projects/urls.py
+++ Django_Portfolio_app/projects/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from projects import views
 
+app_name = 'projects'
+
 urlpatterns = [
-    path('', views.all_projects),
-    path('<int:pk>', views.projects_detail),
+    path('', views.all_projects, name='all_projects'),
+    path('<int:pk>', views.projects_detail, name='project_detail'),
 ]
 
 
