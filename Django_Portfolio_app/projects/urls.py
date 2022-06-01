169b985ca3e842fb4ba0796ca92827c33e230f57
jamescarr0 <jamescarr0@hotmail.com>
2022-06-01 14:45:38 +0100

    Django templates comlete

--- Django_Portfolio_app/projects/urls.py
+++ Django_Portfolio_app/projects/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from projects import views
 
-
 urlpatterns = [
-    path('', views.ExampleClassView.as_view(html='projects/index.html'))
+    path('', views.all_projects),
+    path('test', views.projects_list),
 ]
+
+
+# # Class Based View
+# urlpatterns = [
+#     path('', views.ExampleClassView.as_view())
+# ]
--- Django_Portfolio_app/projects/views.py
+++ Django_Portfolio_app/projects/views.py
@@ -1,12 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+from projects.models import Projects
 
 # Create your views here.
+def all_projects(request):
+    # Query the db to return all project objects.
+    projects = Projects.objects.all()
+    context = {'projects': projects}
+    return render(request, 'projects/all_projects.html', context=context)
+
 def projects_list(request):
     return render(request, 'projects/index.html')
 
 class ExampleClassView(View):
-    html=None
+    """ Example Class Based View """
+    html='projects/index.html'
     def get(self, request):
         return render(request, self.html)
